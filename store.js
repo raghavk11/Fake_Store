809d610f6f09efbd110b3756515db84eca075050
Raghav Kohli <raghavkohli011@gmail.com>
2024-05-23 18:34:28 +1000

    Update store.js

--- store.js
+++ store.js
@@ -1,10 +1,10 @@
-// In your store.js file
-
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './Features/Cart/CartSlice';
+import authReducer from './Features/Auth/AuthSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    auth: authReducer,
   },
 });
